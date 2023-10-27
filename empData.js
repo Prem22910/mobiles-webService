4f0ff9c6b0cea45d0184a2a1f0b2dc5b668a7b36
Prem Mishra <premmishra.mail@gmail.com>
2023-10-27 17:35:16 +0530

    Add files via upload

---
+++ empData.js
@@ -0,0 +1,18 @@
+let employees = [
+  {empCode: 1451,name: "Jack",department: "Finance",designation: "Manager",salary: 52500,gender: "Male"},
+  {empCode: 1029,name: "Steve",department: "Technology",designation: "Manager",salary: 71000,gender: "Male"},
+  {empCode: 1891,name: "Anna",department: "HR",designation: "Manager",salary: 55100,gender: "Female"},
+  {empCode: 1322,name: "Kathy",department: "Operations",designation: "Manager",salary: 49200,gender: "Female"},
+  {empCode: 1367,name: "Bob",department: "Marketing",designation: "Manager",salary: 39000,gender: "Male"},
+  {empCode: 1561,name: "George",department: "Finance",designation: "Trainee",salary: 22500,gender: "Male"},
+  {empCode: 1777,name: "Harry",department: "Technology",designation: "Trainee",salary: 31000,gender: "Male"},
+  {empCode: 1606,name: "Julia",department: "HR",designation: "Manager",salary: 25100,gender: "Female"},
+  {empCode: 1509,name: "Kristina",department: "Operations",designation: "Trainee",salary: 19200,gender: "Female"},
+  {empCode: 1533,name: "William",department: "Marketing",designation: "Trainee",salary: 16200,gender: "Male"},
+  {empCode: 1161,name: "Stephen",department: "Finance",designation: "VP",salary: 82500,gender: "Male"},
+  {empCode: 1377,name: "Winston",department: "Technology",designation: "VP",salary: 91000,gender: "Male"},
+  {empCode: 1206,name: "Victoria",department: "HR",designation: "Manager",salary: 65100,gender: "Female"},
+  {empCode: 1809,name: "Pamela",department: "Operations",designation: "VP",salary: 78600,gender: "Female"},
+];
+
+module.exports.empData = employees;
